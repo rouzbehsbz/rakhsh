1833019338f1122a60203d4f38b9bac37d231169
Rouzbeh Sabzehei <rouzbehsbz@gmail.com>
2026-07-14 10:22:42 +0330

    chore: refactor main for better readibility

--- cmd/core/main.go
+++ cmd/core/main.go
@@ -17,7 +17,7 @@ import (
 func main() {
 	config, err := common.NewConfig(true)
 	if err != nil {
-		panic(err)
+		log.Fatalf("failed to load config: %v", err)
 	}
 
 	snowflake.SetMachineID(config.MachineId)
@@ -30,65 +30,56 @@ func main() {
 		config.PostgresShard3Url,
 	}, celebritiesShard, config.PostgresMaxConnections)
 	if err != nil {
-		panic(err)
+		log.Fatalf("failed to init postgres: %v", err)
 	}
 
-	rabbitmq, err := rabbitmq.NewRabbitmq(config.RabbitmqUrl)
+	rabbit, err := rabbitmq.NewRabbitmq(config.RabbitmqUrl)
 	if err != nil {
-		panic(err)
+		log.Fatalf("failed to init rabbitmq: %v", err)
 	}
 
 	redis, err := redis.NewRedis(config.RedisUrl, config.RedisPassword, config.RedisMaxConnections)
 	if err != nil {
-		panic(err)
+		log.Fatalf("failed to init redis: %v", err)
 	}
 
 	clientRepository := client.NewClientRepository(postgres, redis)
-	messageRepository := message.NewMessageRepository(postgres, rabbitmq, redis)
+	messageRepository := message.NewMessageRepository(postgres, rabbit, redis)
 
 	operatorService := operator.NewOperatorService()
-	operatorService.RegisterOperator(operator.NewDummyOperator())
-	operatorService.RegisterOperator(operator.NewDummyOperator())
-	operatorService.RegisterOperator(operator.NewDummyOperator())
+	for i := 0; i < 3; i++ {
+		operatorService.RegisterOperator(operator.NewDummyOperator())
+	}
 
 	clientService := client.NewClientService(clientRepository)
 	messageService := message.NewMessageService(postgres, clientRepository, messageRepository, operatorService)
 
-	clientHandler := client.NewClientHandler(clientService)
-	messageHandler := message.NewMessageHandler(messageService)
-
-	if err := rabbitmq.AddQueue(common.PendingMessagesQueueName, messageService.ProcessPendingMessage); err != nil {
-		panic(err)
-	}
-	if err := rabbitmq.AddQueue(common.SubmittedMessagesQueueName, messageService.ProcessSubmittedMessage); err != nil {
-		panic(err)
-	}
-	if err := rabbitmq.AddQueue(common.DeliveredMessageQueueName, messageService.ProcessDeliveredMessage); err != nil {
-		panic(err)
-	}
-	if err := rabbitmq.AddQueue(common.RejectedMessagesQueueName, messageService.ProcessRejectedMessage); err != nil {
-		panic(err)
+	queues := []struct {
+		name    string
+		handler rabbitmq.QueueHandler
+	}{
+		{common.PendingMessagesQueueName, messageService.ProcessPendingMessage},
+		{common.SubmittedMessagesQueueName, messageService.ProcessSubmittedMessage},
+		{common.DeliveredMessageQueueName, messageService.ProcessDeliveredMessage},
+		{common.RejectedMessagesQueueName, messageService.ProcessRejectedMessage},
 	}
 
-	if err := rabbitmq.StartQueueConsumers(common.PendingMessagesQueueName, config.RabbitmqMaxWorkers); err != nil {
-		panic(err)
-	}
-	if err := rabbitmq.StartQueueConsumers(common.SubmittedMessagesQueueName, config.RabbitmqMaxWorkers); err != nil {
-		panic(err)
-	}
-	if err := rabbitmq.StartQueueConsumers(common.DeliveredMessageQueueName, config.RabbitmqMaxWorkers); err != nil {
-		panic(err)
-	}
-	if err := rabbitmq.StartQueueConsumers(common.RejectedMessagesQueueName, config.RabbitmqMaxWorkers); err != nil {
-		panic(err)
+	for _, q := range queues {
+		if err := rabbit.AddQueue(q.name, q.handler); err != nil {
+			log.Fatalf("failed to add queue %s: %v", q.name, err)
+		}
+		if err := rabbit.StartQueueConsumers(q.name, config.RabbitmqMaxWorkers); err != nil {
+			log.Fatalf("failed to start consumers for %s: %v", q.name, err)
+		}
 	}
 
 	server := api.NewServer(config.Host, config.Port, api.RootHandlers{
-		ClientHandler:  clientHandler,
-		MessageHandler: messageHandler,
+		ClientHandler:  client.NewClientHandler(clientService),
+		MessageHandler: message.NewMessageHandler(messageService),
 	})
 
+	log.Printf("Starting server on %s:%d ...", config.Host, config.Port)
 	if err := server.Run(); err != nil {
-		log.Fatal(err)
+		log.Fatalf("server terminated unexpectedly: %v", err)
 	}
 }
